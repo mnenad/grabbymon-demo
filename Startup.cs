6f512d86a62d5691219a2dfc5ba00469b9812a1d
Chris Umbel <chris@chrisumbel.com>
2016-07-26 02:34:21 +0000

    formatting

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ namespace StatlerWaldorfCorp.Grabbymon {
 
         public void ConfigureServices(IServiceCollection services)
         {
-	        services.AddMvc();
+            services.AddMvc();
         }        
     }
 }
