bcc6875a00deefefc455d2311fdd65eca0a687ef
Chris Umbel <chris@chrisumbel.com>
2016-07-26 00:55:29 +0000

    removed temporary routing

--- Controllers/MonstersController.cs
+++ Controllers/MonstersController.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace StatlerWaldorfCorp.Grabbymon.Controllers {
-    [Route("api/[controller]")]
     public class MonsterController : Controller {
         public MonsterController() {
         }
 
+        [Route("/")]
         public virtual IActionResult Get() {
             return this.Ok("Hello, world!");
         }
